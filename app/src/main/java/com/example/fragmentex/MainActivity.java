7d9ff0d899c617c2339c23800ba9f5c7671ba823
israel <israel.abramovich@gmail.com>
2024-12-25 16:39:12 +0200

    First commit

---
+++ app/src/main/java/com/example/fragmentex/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.fragmentex;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new MyFragment());
+        fragmentTransaction.commit();
+
+    }
+}
